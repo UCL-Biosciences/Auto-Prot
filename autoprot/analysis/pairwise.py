ceea966a387bf4567a10268856cbc5c743583ed1
jdgilbert245 <james.d.gilbert@ucl.ac.uk>
2026-03-13 14:27:34 +0000

    pathway universe comes from config

--- autoprot/analysis/pairwise.py
+++ autoprot/analysis/pairwise.py
@@ -218,7 +218,11 @@ def enrichment_analysis(
     #     pathway_query_genes = [str(gene).split("__")[0] for gene in pathway_query_genes]
     # pathway database can be REAC, GO or KEGG. Also less common but available: CORUM, HPA, TF and MIRNA
     # defaults to REAC
-    source = ["KEGG"]
+    source = config.get("enrichment_pathways")
+    # Normalise to list for gprofiler if just a single source specified as string
+    if isinstance(source, str):
+        source = [source]
+    #source = ["KEGG"]
     plot_title = "Pathway Enrichment (" + source[0] + ") for treatments \n " + pair_name
     # p value threshold defaults to 0.05
     p_threshold = 0.05
